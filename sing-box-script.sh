76b41a9517b49c994e01fdc7450fa1e79c55c24a
Roman Chervyakov <roman.chervyakov@travelata.ru>
2025-11-30 01:17:24 +0300

    usage default service, switch from acme to local certs

--- sing-box-script.sh
+++ sing-box-script.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+set -e
+
 # Проверка, был ли передан домен
 if [ -z "$1" ]; then
     echo "Использование: $0 <домен>"
@@ -60,7 +62,7 @@ cat <<EOL > /tmp/new_conf.json
     "log": {
         "disabled": false,
         "level": "info",
-        "output": "/root/box.log",
+        "output": "/var/lib/box.log",
         "timestamp": true
     },
 
@@ -113,13 +115,8 @@ cat <<EOL > /tmp/new_conf.json
             "tls": {
                 "enabled": true,
                 "server_name": "$DOMAIN",
-                "acme" : {
-                    "domain": "$DOMAIN",
-                    "email": "$user_email"
-                },
-                "alpn": [
-                    "h3"
-                ]
+                "certificate_path": "/etc/sing-box/cert.pem",
+                "key_path": "/etc/sing-box/key.pem"
             }
         },
         {
@@ -138,7 +135,8 @@ cat <<EOL > /tmp/new_conf.json
                 "acme": {
                    "domain": "$DOMAIN",
                    "email": "$user_email"
-                }
+                },
+                "alpn": ["h2"]
             }
         }
     ],
@@ -153,15 +151,19 @@ cat <<EOL > /tmp/new_conf.json
 EOL
 
 # Копирование нового файла конфигурации
-cp /tmp/new_conf.json /etc/sing-box/config.json
+cp /tmp/new_conf.json /etc/sing-box/server.json
 
 # Удаление временного файла
 rm /tmp/new_conf.json
 
+# Создание файла логов
+touch /var/lib/box.json
+chmod sing-box: /var/lib/box.log
+
 echo "Ключи и сертификат успешно сохранены."
 echo "Конфигурация обновлена."
 echo -e "Проверка конфигурации.\n"
-sing-box check -c /etc/sing-box/config.json
+sing-box check -c /etc/sing-box/server.json
 
 echo -e "\nДанные для аутентификации:"
 echo -e "Пароли
@@ -173,26 +175,12 @@ hysteria: $hysteria_port
 naive: $naive_port
 "
 
-# Создание демона systemd для sing-box
-touch /etc/systemd/system/sing-box.service
-
-echo "
-[Unit]
-Description=Sing-Box service
-After=network.target
-
-[Service]
-ExecStart=/usr/bin/sing-box run -c /etc/sing-box/config.json
-Restart=on-failure
-
-[Install]
-WantedBy=multi-user.target" > /etc/systemd/system/sing-box.service
-systemctl daemon-reload
-
 echo -e "\n\nЗапуск и проверка работоспособности демона Sing-Box.\n"
 
-systemctl start sing-box.service 
+/usr/bin/sing-box check --config /etc/sing-box/server.json && /usr/bin/sing-box format --config /etc/sing-box/server.json -w
+
+systemctl start sing-box@server.service
 
 echo -e '\n\n'
 
-systemctl status sing-box.service
+systemctl status sing-box@server.service
